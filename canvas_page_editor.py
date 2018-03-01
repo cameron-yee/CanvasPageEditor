8d036e6a4053c581465d6bf388a366cc0369cbe4
cameron-yee <yee.camero23@gmail.com>
2018-03-01 13:22:01 -0700

    Fixed functionality to allow our file structure and naming conventions to work

--- canvas_page_editor.py
+++ canvas_page_editor.py
@@ -4,15 +4,54 @@ import json
 import pprint
 import re
 import os
+import argparse
+import auth
+from collections import OrderedDict
 
 def selectAction():
-    questions = [
-        inquirer.List('action',
-                      message="What do you want to do?",
-                      choices=['Create course', 'Create module', 'Create page', 'Update page'],
-        ),
-    ]
-    answers = inquirer.prompt(questions)
+    parser = argparse.ArgumentParser(description='Pick action.')
+
+    commands = {
+        'uc': updateCoursePages,
+
+    }
+    
+    parser.add_argument('command', choices=commands.keys())
+    args = parser.parse_args()
+
+    commands[args.command]()
+
+
+#Gets html body content for each page in course
+def getHtmlData(html_files):
+    html_data = [] 
+    for page in html_files:
+        with open(page, 'r') as f: 
+            contents = f.read()
+            html_data.append(contents) 
+        f.close()
+    return html_data
+
+
+def storeHtmlData(html_files):
+    html_data = getHtmlData(html_files)
+    html_dict = {}
+
+    for i in range(len(html_files)):
+        key = re.search('\d+_(\S+.html$)', html_files[i])
+        #sets the file 'url' without the 00_ as the key
+        #stores both the full html file and the html data from the file
+        html_dict[str(key.group(1))] = html_files[i], html_data[i]
+        #sorts dictionary alphabetically by key
+        sorted_dict = OrderedDict(sorted(html_dict.items(), key=lambda t: t[0]))
+    return sorted_dict
+
+
+#NOT SURE IF THIS FUNCTION IS NECESSARY
+#Sorts the html files in the same order that Canvas LMS API returns them
+def sortHtmlFiles(paths):
+    html_files = glob(str(path) + '/*.html')
+
 
 #Gets every page in a course and appends the page url to a list
 def getCoursePages(course_id, headers):
@@ -47,30 +86,23 @@ def getCoursePages(course_id, headers):
     return pages
 
 
-#Gets html body content for each page in course
-def getHtmlData(pages):
-    html_data = []
-    for page in pages:
-        with open(page + '.html', 'r') as f:
-            contents = f.read
-            html_data.append(contents) 
-            f.close()
-    print(html_data)
-
-
-#Updates html body content for each Canvas page in a given coures given the directory where the html files are stored
+#Update content for each Canvas page in a given coures given the directory where the html files are stored
 def updateCoursePages(course_id, headers, html_files):
     pages = getCoursePages(course_id, headers)
+    sorted_dict = storeHtmlData(html_files)
     count = 0
 
-    assert(len(pages) == len(html_files))
-    for page_url in pages:
-        updateIndividualPage(course_id, headers, page_url, html_files[count])
+    assert(len(pages) == len(sorted_dict))
+    for key, value in sorted_dict.items():
+        page_url = pages[count]
+        html_content = value[1]
+        updateIndividualPage(course_id, headers, page_url, value[0], value[1])
         count += 1
+    print('Success')
 
 
 #Prints json object for individual page
-def getPageInformation(course_id, page_url, headers):
+def getPageInformation(course_id, headers, page_url):
     url = url_base + course_id + '/pages/' + page_url
     r = requests.get(url, headers=headers)
     r.json
@@ -78,34 +110,53 @@ def getPageInformation(course_id, page_url, headers):
     print(json.dumps(x, sort_keys=True, indent=4))
 
 
-#Updates html body content for given Canvas page and html file
-def updateIndividualPage(course_id, headers, page_url, file_name):
+#Updates html body content for given Canvas page and html file, for updating entire course
+def updateIndividualPage(course_id, headers, page_url, file_name, html_content):
     url = url_base + course_id + '/pages/' + page_url
-    with open(file_name, 'r') as f:
-        html = f.read()
-    data = [('wiki_page[body]', html),]
+    data = [('wiki_page[body]', html_content),]
     r = requests.put(url, headers=headers, data=data)
 
 
+#TODO: Figure out multiple function signature work around
+#Alternate function signature that retrieves the html content
+# def updateIndividualPage(course_id, headers, page_url, file_name):
+#     url = url_base + course_id + '/pages/' + page_url
+#     with open(file_name, 'r') as f:
+#         html = f.read()
+#     data = [('wiki_page[body]', html),]
+#     r = requests.put(url, headers=headers, data=data)
+
+
 #Create a new course in Canvas
-def createNewCourse(course_name, headers):
+def createNewCourse(headers, course_name):
     url = 'https://***REMOVED***.instructure.com/api/v1/accounts/1/courses'
     data = [('course[name]', course_name),]
     r = requests.post(url, headers=headers, data=data)
 
 
 #Update course name
-def updateCourseName(new_course_name, course_id, headers):
+def updateCourseName(course_id, headers, new_course_name):
     url = url_base + course_id
     data = [('course[name]', new_course_name),]
     r = requests.put(url, headers=headers, data=data)
 
 
 #Create a new module in a Canvas Course
-def createNewModule(module_name, course_id, headers):
+def createNewModule(course_id, headers, module_name):
     url = url_base + course_id + '/modules'
     data = [('module[name]', module_name),]
     r = requests.post(url, headers=headers, data=data)
+
+
+#Lists every item's information 
+def listModuleItems(course_id, headers, module_name):
+    module_id = getModuleId(course_id, headers, module_name)
+    url = url_base + course_id + '/modules/' + module_id + '/items'
+    data = [('module[name]', module_name),]
+    r = requests.get(url, headers=headers, data=data)
+    r.json
+    x = json.loads(r.text)
+    print(json.dumps(x, sort_keys=True, indent=4))
 
 
 #Returns the id for the wanted module
@@ -146,9 +197,7 @@ def createNewPage(course_id, headers, page_name, html_file):
 
 #Gets token from hidden file so it will not show up on Git
 def getAccessToken():
-    with open('Token.txt', 'r') as f:
-        token = f.read()
-        f.close()
+    token = auth.token
     return token
 
 
@@ -172,18 +221,22 @@ if __name__  == '__main__':
     access_token = getAccessToken()
     headers = {"Authorization": "Bearer " + access_token}
     course_id = '122'
-    page_url = 'test'
+    # page_url = 'test'
     url_base = 'https://***REMOVED***.instructure.com/api/v1/courses/'
-    page_titles = ['Table of Contents','System Requirements','Using the Course','Lesson 1: Water All Around Us','Lesson 2: Surface Water','Lesson 3: Groundwater','Lesson 4: Watersheds','Lesson 5: Atmosphere','Lesson 6: Oceans','Lesson 7: Human Impacts on Water Resources']
-    html_files = glob('/Users/cameronyee/Desktop/canvas/courses/mhs/courses/mhs_te/*.html')
+    # page_titles = ['Table of Contents','System Requirements','Using the Course','Lesson 1: Water All Around Us','Lesson 2: Surface Water','Lesson 3: Groundwater','Lesson 4: Watersheds','Lesson 5: Atmosphere','Lesson 6: Oceans','Lesson 7: Human Impacts on Water Resources']
+    html_files = glob('/Users/cameronyee/Desktop/canvas/courses/mhs/courses/te/*.html')
     module_name = 'Teacher Guide'
-
-    print(html_files)
     # getCoursePages(course_id, headers)
     updateCoursePages(course_id, headers, html_files)
+    # getPageInformation(course_id, 'lesson-1-water-all-around-us', headers)
+    # listModuleItems('Teacher Guide', course_id, headers)
+    # print(html_files)
+    # storeHtmlData(html_files)
+    # getHtmlData(html_files)
+    # getCoursePages(course_id, headers)
 
 #NEED UPDATING:
-    # selectAction()
+    # selectAction
 
 #FUNCTIONAL:
     # getCoursePages(course_id, headers)
