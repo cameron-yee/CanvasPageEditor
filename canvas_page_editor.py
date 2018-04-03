412c97156b45e30ae8a5c226aab74144bb13504d
cameron-yee <yee.camero23@gmail.com>
2018-04-03 15:49:10 -0600

    Added media server update to CLI option in canvas_page_editor

--- canvas_page_editor.py
+++ canvas_page_editor.py
@@ -8,6 +8,7 @@ import argparse
 import auth
 from collections import OrderedDict
 from colors import bcolors
+from media_server_update import *
 
 #CLI command definitions
 def selectAction():
@@ -19,6 +20,8 @@ def selectAction():
     #   'ind': updateIndividualPage,
     #}
 
+    parser.add_argument('cid', help="Enter course ID", default="", type=str)
+
     parser_uc = subparsers.add_parser('uc', help='Commands to use when updateCoursePages')
     parser_uc.add_argument('top_directory', help="Enter the directory that contains the html files, or subfolders that contain the html files", type=str)
     parser_uc.set_defaults(which='uc')
@@ -27,9 +30,15 @@ def selectAction():
     parser_ind.add_argument('page_url', help="Enter the last section of the url that the page is on", type=str)
     parser_ind.add_argument('file_path', help="Enter the path to file on your local device", type=str)
     parser_ind.set_defaults(which='ind')
+
+    parser_static = subparsers.add_parser('static', help='Commands to use when pushing static files to media server')
+#    parser_static.add_argument('--css', help="Update css file on media server", type=str)
+#    parser_static.add_argument('--js', help="Update js file on media server", type=str)
+#    parser_static.add_argument('--html', help="Update html menu on media server", type=str)
+    parser_static.add_argument('course_prefix', help="Enter the prefix of the course", type=str)
+    parser_static.set_defaults(which='static')
     
     #parser.add_argument('command', help="Choose which function to run", choices=commands.keys())
-    parser.add_argument('cid', help="Enter course ID", default="", type=str)
 
     args = parser.parse_args()
     return args
@@ -305,7 +314,12 @@ if __name__  == '__main__':
        updateCoursePages(args.top_directory)
     if args.which == 'ind':
        updateIndividualPage(course_id, headers, args.page_url, args.file_path)
+    if args.which == 'static':
+        course = args.course_prefix
+        upload_all(course)
+        print('Updated {} static files'.format(course))
 
+    #update_all(course)
 
 
 
