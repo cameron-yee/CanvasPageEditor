de27bceff8ae38f7aa71c963a096ecafe48aae16
cameron-yee <yee.camero23@gmail.com>
2018-04-03 15:49:10 -0600

    Added media server update to CLI option in canvas_page_editor

--- media_server_update.py
+++ media_server_update.py
@@ -7,7 +7,6 @@ import re
 def upload_css(course):
     session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
     session.cwd('media/canvas/{}/css'.format(course))
-    print(session.pwd())
     f = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/styles/css/concat/concat.css'.format(course)
     local_file = copyfile(f, './concat.css')
     session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
@@ -18,7 +17,6 @@ def upload_css(course):
 def upload_js(course):
     session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
     session.cwd('media/canvas/{}/js'.format(course))
-    print(session.pwd())
     f = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/js/concat/concat.js'.format(course)
     local_file = copyfile(f, './concat.js')
     session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
@@ -29,10 +27,8 @@ def upload_js(course):
 def upload_html(course):
     session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
     session.cwd('media/canvas/{}/html'.format(course))
-    print(session.pwd())
     html_folder = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/html/*.html'.format(course)
     html_menus = glob(html_folder)
-    print(html_menus)
     for menu in html_menus:
         r = re.search('[^/]+.html$', menu)
         menu_name = r.group(0)
@@ -46,9 +42,5 @@ def upload_all(course):
     upload_css(course)
     upload_js(course)
     upload_html(course)
-
-if __name__ == '__main__':
-    upload_all('mss')
-
 
 
