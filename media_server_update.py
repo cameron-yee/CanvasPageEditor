3cc54a25f94ce3cced84c6dd20e3617981898710
cameron-yee <yee.camero23@gmail.com>
2018-05-21 17:17:26 -0600

    Added except statements to catch no file errors

--- media_server_update.py
+++ media_server_update.py
@@ -6,47 +6,56 @@ import re
 from colors import bcolors
 
 def upload_css(course):
-    session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
-    session.cwd('media/canvas/{}/css'.format(course))
-    f = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/styles/css/concat/concat.css'.format(course)
-    local_file = copyfile(f, './concat.css')
-    session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
-    os.remove('./concat.css')
-    session.quit()
+    try: 
+        session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
+        session.cwd('media/canvas/{}/css'.format(course))
+        f = '/Users/cyee/Desktop/canvas/courses/{}/resources/styles/css/concat/concat.css'.format(course)
+        local_file = copyfile(f, './concat.css')
+        session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
+        os.remove('./concat.css')
+        session.quit()
+        print(bcolors.WARNING + 'CSS Uploaded' + bcolors.ENDC)
+    except ftplib.error_perm as err:
+        print(bcolors.FAIL + 'No concat.css file' + bcolors.ENDC)
 
 
 def upload_js(course):
-    session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
-    session.cwd('media/canvas/{}/js'.format(course))
-    f = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/js/concat/concat.js'.format(course)
-    local_file = copyfile(f, './concat.js')
-    session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
-    os.remove('./concat.js')
-    session.quit()
+    try:
+        session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
+        session.cwd('media/canvas/{}/js'.format(course))
+        f = '/Users/cyee/Desktop/canvas/courses/{}/resources/js/concat/concat.js'.format(course)
+        local_file = copyfile(f, './concat.js')
+        session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
+        os.remove('./concat.js')
+        session.quit()
+        print(bcolors.WARNING + 'JS Uploaded' + bcolors.ENDC)
+    except ftplib.error_perm as err:
+        print(bcolors.FAIL + 'No concat.js file' + bcolors.ENDC)
 
 
 def upload_html(course):
-    session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
-    session.cwd('media/canvas/{}/html'.format(course))
-    html_folder = '/Users/cameronyee/Desktop/canvas/courses/{}/resources/html/*.html'.format(course)
-    html_menus = glob(html_folder)
-    for menu in html_menus:
-        r = re.search('[^/]+.html$', menu)
-        menu_name = r.group(0)
-        local_file = copyfile(menu, './{}'.format(menu_name))
-        session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
-        os.remove('./{}'.format(menu_name))
-    session.quit()
+    try:
+        session = ftplib.FTP('***REMOVED***', '***REMOVED***', '***REMOVED***')
+        session.cwd('media/canvas/{}/html'.format(course))
+        html_folder = '/Users/cyee/Desktop/canvas/courses/{}/resources/html/*.html'.format(course)
+        html_menus = glob(html_folder)
+        for menu in html_menus:
+            r = re.search('[^/]+.html$', menu)
+            menu_name = r.group(0)
+            local_file = copyfile(menu, './{}'.format(menu_name))
+            session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
+            os.remove('./{}'.format(menu_name))
+        session.quit()
+        print(bcolors.WARNING + 'HTML Uploaded' + bcolors.ENDC)
+    except ftplib.error_perm as err:
+        print(bcolors.FAIL + 'No HTML file' + bcolors.ENDC)
 
 
 def upload_all(course):
     upload_css(course)
-    print(bcolors.WARNING + 'CSS Uploaded' + bcolors.ENDC)
     upload_js(course)
-    print(bcolors.WARNING + 'JS Uploaded' + bcolors.ENDC)
     upload_html(course)
-    print(bcolors.WARNING + 'HTML Uploaded' + bcolors.ENDC)
-    print(bcolors.FAIL + 'DONE' + bcolors.ENDC)
+    print(bcolors.OKBLUE + 'DONE' + bcolors.ENDC)
 
 if __name__ == '__main__':
     upload_css('3dmss')
