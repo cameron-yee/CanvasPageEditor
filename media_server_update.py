55efad244888568f76a17f83dcb03890c6c046c7
cameron-yee <yee.camero23@gmail.com>
2018-06-26 17:01:32 -0600

    Added sftp to media server update python

--- media_server_update.py
+++ media_server_update.py
@@ -4,13 +4,102 @@ import os
 from glob import glob
 import re
 from colors import bcolors
-from auth import server, user, password
+from auth import server, user, password, remote_base, local_base
+import paramiko
 
-def upload_css(course):
+def upload_css_sftp(course, sub_course=None):
+    try:
+        port = 22
+
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
+        local_css_path = '{}/{}/resources{}/styles/css/concat/concat.css'.format(local_base, course, sub_course)
+        remote_css_path = '{}/{}{}/css/concat.css'.format(remote_base, course, sub_course)
+
+        transport = paramiko.Transport((server, port))
+        transport.connect(username=user, password=password)
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.put(local_css_path, remote_css_path)
+
+        sftp.close()
+        transport.close()
+        print(bcolors.WARNING + 'CSS Uploaded' + bcolors.ENDC)
+    except:
+        print(bcolors.FAIL + 'No concat.css file' + bcolors.ENDC)
+
+
+def upload_js_sftp(course, sub_course=None):
+    try:
+        port = 22
+
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
+        local_js_path = '{}/{}/resources{}/js/concat/concat.js'.format(local_base, course, sub_course)
+        remote_js_path = '{}/{}{}/js/concat.js'.format(remote_base, course, sub_course)
+
+        transport = paramiko.Transport((server, port))
+        transport.connect(username=user, password=password)
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.put(local_js_path, remote_js_path)
+
+        sftp.close()
+        transport.close()
+        print(bcolors.WARNING + 'JS Uploaded' + bcolors.ENDC)
+    except:
+        print(bcolors.FAIL + 'No concat.js file' + bcolors.ENDC)
+
+
+def upload_html_sftp(course, sub_course=None):
+    try:
+        port = 22
+
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
+        html_folder = '{}/{}/resources{}/html/*.html'.format(local_base, course, sub_course)
+        html_menus = glob(html_folder)
+
+        transport = paramiko.Transport((server, port))
+        transport.connect(username=user, password=password)
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        for menu in html_menus:
+            r = re.search('[^/]+.html$', menu)
+            menu_name = r.group(0)
+            remote_html_path = '{rb}/{c}{sc}/html/{m}'.format(rb=remote_base, c=course, sc=sub_course, m=menu_name)
+            sftp.put(menu, remote_html_path)
+            print(bcolors.WARNING + '{} Uploaded'.format(menu_name) + bcolors.ENDC)
+
+        print(bcolors.FAIL + 'No HTML file' + bcolors.ENDC) if html_menus == [] else print('')
+
+        sftp.close()
+        transport.close()
+    except:
+        print(bcolors.FAIL + 'No HTML file' + bcolors.ENDC)
+        raise
+
+
+def upload_css(course, sub_course=None):
     try: 
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
         session = ftplib.FTP(server, user, password)
-        session.cwd('media/canvas/{}/css'.format(course))
-        f = '/Users/cyee/Desktop/canvas/courses/{}/resources/styles/css/concat/concat.css'.format(course)
+        session.cwd('{}/{}{}/css'.format(remote_base, course, sub_course))
+        f = '{}/{}/resources{}/styles/css/concat/concat.css'.format(local_base, course, sub_course)
         local_file = copyfile(f, './concat.css')
         session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
         os.remove('./concat.css')
@@ -20,11 +109,16 @@ def upload_css(course):
         print(bcolors.FAIL + 'No concat.css file' + bcolors.ENDC)
 
 
-def upload_js(course):
+def upload_js(course, sub_course=None):
     try:
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
         session = ftplib.FTP(server, user, password)
-        session.cwd('media/canvas/{}/js'.format(course))
-        f = '/Users/cyee/Desktop/canvas/courses/{}/resources/js/concat/concat.js'.format(course)
+        session.cwd('{}/{}{}/js'.format(remote_base, course, sub_course))
+        f = '{}/{}/resources{}/js/concat/concat.js'.format(local_base, course, sub_course)
         local_file = copyfile(f, './concat.js')
         session.storbinary('STOR {}'.format(local_file), open(local_file,'rb'))
         os.remove('./concat.js')
@@ -34,11 +128,16 @@ def upload_js(course):
         print(bcolors.FAIL + 'No concat.js file' + bcolors.ENDC)
 
 
-def upload_html(course):
+def upload_html(course, sub_course=None):
     try:
+        if sub_course is not None:
+            sub_course = '/{}'.format(sub_course)
+        else:
+            sub_course = ''
+
         session = ftplib.FTP(server, user, password)
-        session.cwd('media/canvas/{}/html'.format(course))
-        html_folder = '/Users/cyee/Desktop/canvas/courses/{}/resources/html/*.html'.format(course)
+        session.cwd('{}/{}{}/html'.format(remote_base, course, sub_course))
+        html_folder = '{}/{}/resources{}/html/*.html'.format(local_base, course, sub_course)
         html_menus = glob(html_folder)
         for menu in html_menus:
             r = re.search('[^/]+.html$', menu)
@@ -52,12 +151,21 @@ def upload_html(course):
         print(bcolors.FAIL + 'No HTML file' + bcolors.ENDC)
 
 
-def upload_all(course):
-    upload_css(course)
-    upload_js(course)
-    upload_html(course)
+def upload_all(course, sub_course=None):
+    upload_css(course, sub_course)
+    upload_css_sftp(course, sub_course)
+    upload_js(course, sub_course)
+    upload_js_sftp(course, sub_course)
+    upload_html(course, sub_course)
+    upload_html_sftp(course, sub_course)
     print(bcolors.OKBLUE + 'DONE' + bcolors.ENDC)
 
+
 if __name__ == '__main__':
-    upload_css('3dmss')
+    #upload_css('vatl')
+    #upload_css_sftp('vatl')
+    #upload_js('vatl')
+    #upload_js_sftp('vatl')
+    #upload_html('vatl')
+    upload_html_sftp('vatl')
 
